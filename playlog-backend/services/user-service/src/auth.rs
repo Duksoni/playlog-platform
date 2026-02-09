f3ce7deb7ea108f4034f7a56af7d7c01ce52c767
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2026-02-09 22:09:29 +0100

    playlog-backend-user-service: Create axum handlers for auth endpoints (login, register, logout, refresh)

--- playlog-backend/services/user-service/src/auth.rs
+++ playlog-backend/services/user-service/src/auth.rs
@@ -1,5 +1,6 @@
 pub mod dto;
 pub mod error;
+pub mod handler;
 pub mod model;
 pub mod password;
 pub mod repository;
@@ -8,6 +9,7 @@ pub mod token;
 
 pub use dto::{LoginRequest, RegisterRequest, RegisterResponse, TokenResponse};
 pub use error::{AuthError, Result};
+pub use handler::router;
 pub use model::{AccountStatus, User};
 pub use password::{hash_password, verify_password};
 pub use repository::{AuthRepository, PostgresAuthRepository};
---
+++ playlog-backend/services/user-service/src/auth/handler.rs
@@ -0,0 +1,161 @@
+use super::{AuthError, LoginRequest, RegisterRequest, RegisterResponse, TokenResponse};
+use crate::app::AppState;
+use api_error::ApiError;
+use axum::{
+    extract::State,
+    http::{header::SET_COOKIE, HeaderMap, StatusCode},
+    response::IntoResponse,
+    routing::post,
+    Json,
+};
+use axum_extra::extract::cookie::{Cookie, CookieJar};
+use axum_macros::debug_handler;
+use cookie::time::Duration;
+use jwt_common::decode_token;
+use std::sync::Arc;
+use utoipa_axum::router::OpenApiRouter;
+use uuid::Uuid;
+use validator::Validate;
+
+pub fn router() -> OpenApiRouter<Arc<AppState>> {
+    OpenApiRouter::new()
+        .route("/login", post(login))
+        .route("/register", post(register))
+        .route("/logout", post(logout))
+        .route("/refresh", post(refresh_tokens))
+}
+
+const REFRESH_TOKEN_COOKIE_NAME: &str = "playlog-refresh-token";
+
+#[utoipa::path(
+       post,
+       path = "/api/auth/login",
+       request_body = LoginRequest,
+       summary = "Login user",
+       responses(
+           (status = 200, description = "Authenticated", body = TokenResponse),
+           (status = 401, description = "Invalid credentials"),
+           (status = 403, description = "User is blocked"),
+           (status = 404, description = "Account doesn't exist or it's deactivated"),
+       ),
+       tag = "Auth",
+)]
+#[debug_handler]
+pub async fn login(
+    State(state): State<Arc<AppState>>,
+    Json(request): Json<LoginRequest>,
+) -> Result<impl IntoResponse, ApiError> {
+    request.validate().map_err(ApiError::from)?;
+    let tokens = state.auth_service.login(request, &state.config).await?;
+
+    let cookie_duration = Duration::days(state.config.refresh_token_validity.num_days());
+    let headers = build_cookie_header(&tokens.1, cookie_duration);
+
+    let mut response = Json(TokenResponse::new(tokens.0)).into_response();
+    response.headers_mut().extend(headers);
+
+    Ok(response)
+}
+
+#[utoipa::path(
+       post,
+       path = "/api/auth/register",
+       request_body = RegisterRequest,
+       summary = "Register new user",
+       responses(
+           (status = 200, description = "Successful register", body = RegisterResponse),
+           (status = 400, description = "Request has missing values, or the values are invalid"),
+           (status = 409, description = "Email or username is already taken"),
+       ),
+       tag = "Auth",
+)]
+#[debug_handler]
+pub async fn register(
+    State(state): State<Arc<AppState>>,
+    Json(request): Json<RegisterRequest>,
+) -> Result<Json<RegisterResponse>, ApiError> {
+    request.validate().map_err(ApiError::from)?;
+    let result = state.auth_service.register(request).await?;
+    Ok(Json(result))
+}
+
+#[utoipa::path(
+    post,
+    path = "/api/auth/logout",
+    summary = "Logout the user",
+    responses(
+        (status = 204, description = "Logged out"),
+        (status = 401, description = "Refresh token missing or invalid"),
+    ),
+    tag = "Auth",
+)]
+#[debug_handler]
+pub async fn logout(
+    State(state): State<Arc<AppState>>,
+    cookie_jar: CookieJar,
+) -> Result<impl IntoResponse, ApiError> {
+    let refresh_token = extract_refresh_token(&cookie_jar)?;
+
+    state.auth_service.revoke_token(&refresh_token).await?;
+
+    let headers = build_cookie_header("", Duration::seconds(0));
+
+    let mut response = StatusCode::NO_CONTENT.into_response();
+    response.headers_mut().extend(headers);
+
+    Ok(response)
+}
+
+#[utoipa::path(
+    post,
+    path = "/api/auth/refresh",
+    summary = "Refresh both tokens",
+    responses(
+           (status = 200, description = "Refreshed token", body = TokenResponse),
+           (status = 401, description = "Refresh token missing or invalid"),
+    ),
+    tag = "Auth",
+)]
+#[debug_handler]
+pub async fn refresh_tokens(
+    State(state): State<Arc<AppState>>,
+    cookie_jar: CookieJar,
+) -> Result<impl IntoResponse, ApiError> {
+    let refresh_token = extract_refresh_token(&cookie_jar)?;
+    let claims = decode_token(&refresh_token, &state.config.jwt_public_key)
+        .map_err(|err| AuthError::TokenError(err.to_string()))?;
+    let user_id =
+        Uuid::parse_str(&claims.sub).map_err(|err| AuthError::TokenError(err.to_string()))?;
+    let tokens = state
+        .auth_service
+        .refresh_token(&state.config, &refresh_token, user_id)
+        .await?;
+
+    let cookie_duration = Duration::days(state.config.refresh_token_validity.num_days());
+    let headers = build_cookie_header(&tokens.1, cookie_duration);
+
+    let mut response = Json(TokenResponse::new(tokens.0)).into_response();
+    response.headers_mut().extend(headers);
+
+    Ok(response)
+}
+
+fn build_cookie_header(refresh_token: &str, max_age: Duration) -> HeaderMap {
+    let mut headers = HeaderMap::new();
+    let cookie = Cookie::build((REFRESH_TOKEN_COOKIE_NAME, refresh_token))
+        .path("/")
+        .max_age(max_age)
+        .http_only(true)
+        .build();
+    headers.append(SET_COOKIE, cookie.to_string().parse().unwrap());
+    headers
+}
+
+fn extract_refresh_token(cookie_jar: &CookieJar) -> Result<String, ApiError> {
+    match cookie_jar.get(REFRESH_TOKEN_COOKIE_NAME) {
+        Some(cookie) => Ok(String::from(cookie.value())),
+        None => Err(ApiError::from(AuthError::TokenError(String::from(
+            "missing refresh token",
+        )))),
+    }
+}
