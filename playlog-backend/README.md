9d143e229ccce36f3c6756653bc77f439dc3cc9a
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2026-02-09 22:22:02 +0100

    playlog-backend-user-service: Update main function with proper starting logic

--- playlog-backend/README.md
+++ playlog-backend/README.md
@@ -1,5 +1,21 @@
 To run sqlx migrations, install sqlx-cli cargo package with: `cargo install sqlx-cli`
 
-Command to run migrations: `sqlx migrate add -r <name>`
+Command to create the database: `sqlx database create`
+
+Command to create migrations: `sqlx migrate add -r <name>`
+
+Command to run migrations: `sqlx migrate run`
 
 Run: `cargo sqlx prepare` to cache the queries for macros.
+
+Generate an RSA key pair via:
+
+```bash
+openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048
+```
+
+```bash
+openssl rsa -pubout -in private.pem -out public.pem
+```
+
+Store the keys in `keys` folder at the root of this project.
--- playlog-backend/services/user-service/src/main.rs
+++ playlog-backend/services/user-service/src/main.rs
@@ -1,20 +1,44 @@
 mod app;
 mod auth;
-mod task;
 mod config;
 mod setup;
+mod task;
 
-use axum::{
-    routing::get,
-    Router,
-};
+use dotenvy::dotenv;
+use std::{net::SocketAddr, sync::Arc};
+use tracing::info;
+
+use app::{build_app, AppState};
+use auth::{AuthService, PostgresAuthRepository};
+use setup::*;
 
 #[tokio::main]
-async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello from User Service!" }));
+async fn main() -> anyhow::Result<()> {
+    init_tracing();
 
-    // run our app with hyper, listening globally on port 3005
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3005").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    dotenv().ok();
+    let environment = config::load_from_environment()?;
+
+    let pool = init_db(&environment.0).await?;
+
+    let auth_repo = Box::new(PostgresAuthRepository::new(pool.clone()));
+    let auth_service = AuthService::new(auth_repo.clone());
+    auth_service.ensure_admin(environment.2).await?;
+
+    task::schedule_token_cleanup(auth_repo.clone());
+
+    let state = Arc::new(AppState::new(environment.1, auth_service));
+    let app = build_app(state);
+
+    let server_address = SocketAddr::from(([0, 0, 0, 0], 3005));
+    let listener = tokio::net::TcpListener::bind(&server_address).await?;
+
+    info!(
+        "Server started. View docs at http://{}/docs",
+        server_address
+    );
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await?;
+    Ok(())
 }
