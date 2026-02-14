cf56576162c29264f9294cc586e24ad84dfa382e
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2026-02-14 12:11:17 +0100

    playlog-backend-user-service: Create UserService

--- playlog-backend/services/user-service/src/users.rs
+++ playlog-backend/services/user-service/src/users.rs
@@ -2,6 +2,7 @@ pub mod dto;
 pub mod error;
 pub mod model;
 pub mod repository;
+pub mod service;
 
 pub use dto::{
     FindUsersQuery, FindUsersResponse, UpdatePasswordRequest, UpdateProfileRequest,
@@ -10,3 +11,4 @@ pub use dto::{
 pub use error::{Result, UserError};
 pub use model::{SimpleUser, UserDetails};
 pub use repository::{PostgresUserRepository, UserRepository};
+pub use service::UserService;
