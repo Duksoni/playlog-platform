47a33ba21e470f8e60b78a39b0a3346818a3dd25
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2026-02-18 21:59:51 +0100

    playlog-backend-api-gateway: Create setup and docs modules
    Setup is similar to the one in user-service
    
    Docs is similar to user-service, but it also includes fetching openapi.json from all the services

---
+++ playlog-backend/services/api-gateway/src/setup.rs
@@ -0,0 +1,46 @@
+use tracing::info;
+use tracing_subscriber::{
+    layer::SubscriberExt,
+    util::SubscriberInitExt
+};
+
+pub fn init_tracing() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!(
+                    "{}=debug,tower_http=debug,axum=trace",
+                    env!("CARGO_CRATE_NAME")
+                )
+                .into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+}
+
+pub async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("Failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    info!("Shutdown signal received, starting graceful shutdown");
+}
